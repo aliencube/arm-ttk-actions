4ca5a015406dff54300cebc1b220e408be84e270
Justin Yoo <justin.yoojh@gmail.com>
2020-09-18 16:53:19 +0900

    Initialise code commit

---
+++ entrypoint.sh
@@ -0,0 +1,27 @@
+#!/bin/sh -l
+
+set -e
+
+# List of args
+# -TemplatePath => $1
+# -File => $2
+# -Test => $3
+# -Skip => $4
+
+_args="-TemplatePath '$1'"
+
+[ "$2" ] && _args="$_args -File '$2'"
+[ "$3" ] && _args="$_args -Test '$3'"
+[ "$4" ] && _args="$_args -Skip '$4'"
+
+echo "Running: sh /arm-ttk/arm-ttk/Test-AzTemplate.sh $_args"
+results=$(sh /arm-ttk/arm-ttk/Test-AzTemplate.sh $_args)
+
+echo "Results: $results"
+# https://github.com/actions/create-release/issues/25
+results="${results//'%'/'%25'}"
+results="${results//$'\n'/'%0A'}"
+results="${results//$'\r'/'%0D'}"
+
+#this sets the output
+echo "::set-output name=results::'$results'"
